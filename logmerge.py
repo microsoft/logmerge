microsoft/logmerge

commit 53ff53ed00107efdcce5cb99f5fe5e2af28515f4

Allow custom timestamp from user input (#8)
* Allow custom timestamp from user input

Example:
python3 logmerge.py file1 file2 -r='(\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d\.\d\d\d) ' -f='%Y-%m-%d %H:%M:%S.%f'

Fixes #4: Timestamp decoding should be extendible from the command line

logmerge.py
@@ -23,6 +23,8 @@ def make_argument_parser():
     parser = argparse.ArgumentParser(description="Merge multiple log files, from different sources, preserving order")
     parser.add_argument("-p", "--prefix", help="List of prefixes to be applied to log entries", nargs="+")
     parser.add_argument("--no-prefix", help="Suppress automatic generation of prefixes", action="store_true")
+    parser.add_argument("-r", "--regex", help="Regex to match and capture the entire timestamp")
+    parser.add_argument("-f", "--format", help="strptime format to convert the captured timestamp")
     # parser.add_argument("--colors", help="List of colors for each log", required=False, nargs="+")
     parser.add_argument("-c", "--colorize", help="Color-code log output", required=False, action="store_true")
     parser.add_argument('logfiles', nargs='+')
@@ -36,6 +38,12 @@ def parse_datetime(line):
     :param line: The log line to be parsed
     :return: Either a datetime or None
     """
+    if custom_pattern:
+        match = custom_pattern.match(line)
+        if match:
+            entry_datetime = datetime.datetime.strptime(match.group(1), custom_format)
+            return entry_datetime
+
     match = iso8601_pattern.match(line)
     if match:
         entry_datetime = datetime.datetime.strptime(match.group(1), '%Y/%m/%d %H:%M:%S.%f')
@@ -168,6 +176,13 @@ def main():
     if args.logfiles is None or len(args.logfiles) < 2:
         print("Requires at least two logfiles")
         exit(1)
+    elif not args.format ^ args.regex:
+        print("Requires both timestamp regex and format or none")
+        exit(1)
+
+    global custom_pattern, custom_format
+    custom_pattern = re.compile(args.regex.encode().decode('unicode_escape'))
+    custom_format = args.format
 
     prefixes = {}
     colors = {}

commit faca000d612bca8e92a23d128856adaed6401e2a

fix program call without custom regex and format
 * '^' does not work on NoneType, crashing the program when custom regex and format were not given
 * fixed by converting regex and format both to bool and test for equality

Bug introduced in 53ff53e and #8

logmerge.py
@@ -178,13 +178,16 @@ def main():
     if args.logfiles is None or len(args.logfiles) < 2:
         print("Requires at least two logfiles")
         exit(1)
-    elif not args.format ^ args.regex:
+    elif bool(args.format) != bool(args.regex):
         print("Requires both timestamp regex and format or none")
         exit(1)
 
     global custom_pattern, custom_format
-    custom_pattern = re.compile(args.regex.encode().decode('unicode_escape'))
-    custom_format = args.format
+    if args.regex:
+        custom_pattern = re.compile(args.regex.encode().decode('unicode_escape'))
+        custom_format = args.format
+    else:
+        custom_pattern, custom_format = None, None
 
     prefixes = collections.defaultdict(lambda: '')
     colorize = args.colorize if sys.stdout.isatty() else False